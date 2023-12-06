93309b7cc9906f29b39c9f20461e6fad8d8a1c91
mbouboul6 <mahjoubbouboul8@gmail.com>
2023-12-06 17:09:36 +0100

    adding ventes class

--- src/main/java/com/example/login/Controllers/Farmer/VentesController.java
+++ src/main/java/com/example/login/Controllers/Farmer/VentesController.java
@@ -3,92 +3,81 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class YourControllerClass {
-    
+
     @FXML
     private TableView<YourDataModelClass> idtableview;
 
     @FXML
-    private TableColumn<YourDataModelClass, String> idrace;
+    private TableColumn<YourDataModelClass, String> idNomDeProduct;
 
     @FXML
-    private TableColumn<YourDataModelClass, String> idnomrace;
+    private TableColumn<YourDataModelClass, String> idQuantite;
 
     @FXML
-    private TableColumn<YourDataModelClass, String> idqantite;
+    private TableColumn<YourDataModelClass, String> idPrix;
 
     @FXML
-    private TableColumn<YourDataModelClass, String> idprix;
+    private TableColumn<YourDataModelClass, String> idDateVentre;
 
     @FXML
-    private TableColumn<YourDataModelClass, String> iddateentre;
+    private TableColumn<YourDataModelClass, String> idClient;
 
-    @FXML
-    private TableColumn<YourDataModelClass, String> iddateentre1;
-
-    @FXML
-    private TextField idserch;
-
-    @FXML
-    private TextField idlabelnomrace;
-
-    @FXML
-    private TextField idlebelqantitefinale;
-
-    @FXML
-    private TextField idlebelprix;
-
-    @FXML
-    private DatePicker idlebeldateentre;
-
-    @FXML
-    private TextField idlebelnomfournisseur;
-
-    @FXML
-    private Button idbuttadd;
-
-    @FXML
-    private Button idbuttref;
-
-    @FXML
-    private Button idbuttmodife;
-
-    @FXML
-    private TextField idlebelprix1;
-
-    // You might have other fields, methods, and initialization logic...
+    // Establish database connection
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/myprojectjavafx";
+    private static final String JDBC_USER = "your_username";
+    private static final String JDBC_PASSWORD = "your_password";
 
     @FXML
     void initialize() {
         // Initialize your TableView columns with corresponding properties from YourDataModelClass
-        idrace.setCellValueFactory(new PropertyValueFactory<>("raceProperty"));
-        idnomrace.setCellValueFactory(new PropertyValueFactory<>("nomRaceProperty"));
-        idqantite.setCellValueFactory(new PropertyValueFactory<>("qantiteProperty"));
-        idprix.setCellValueFactory(new PropertyValueFactory<>("prixProperty"));
-        iddateentre.setCellValueFactory(new PropertyValueFactory<>("dateEntreProperty"));
-        iddateentre1.setCellValueFactory(new PropertyValueFactory<>("dateEntre1Property"));
+        idNomDeProduct.setCellValueFactory(new PropertyValueFactory<>("nomDeProductProperty"));
+        idQuantite.setCellValueFactory(new PropertyValueFactory<>("quantiteProperty"));
+        idPrix.setCellValueFactory(new PropertyValueFactory<>("prixProperty"));
+        idDateVentre.setCellValueFactory(new PropertyValueFactory<>("dateVentreProperty"));
+        idClient.setCellValueFactory(new PropertyValueFactory<>("clientProperty"));
+
+        // Load data from the database
+        loadDataFromDatabase();
 
         // Set up any other initialization logic...
 
         // Add event handlers for buttons or other interactive elements
-        idbuttadd.setOnMouseClicked(this::handleAddButton);
-        idbuttref.setOnMouseClicked(this::handleEditButton);
-        idbuttmodife.setOnMouseClicked(this::handleDeleteButton);
+        // (Replace these with your actual event handlers)
+        idtableview.setOnMouseClicked(this::handleTableClick);
     }
 
-    // Define methods for handling button clicks or other events
-    private void handleAddButton(MouseEvent event) {
-        // Logic for handling the "Add" button click
+    private void loadDataFromDatabase() {
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM ventes");
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                // Create YourDataModelClass objects and add them to the TableView
+                YourDataModelClass dataModel = new YourDataModelClass(
+                        resultSet.getString("Nom_De_Product"),
+                        resultSet.getString("Quantite"),
+                        resultSet.getString("Prix"),
+                        resultSet.getString("Date_Ventre"),
+                        resultSet.getString("Client")
+                );
+
+                idtableview.getItems().add(dataModel);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Handle exceptions appropriately, log or show a user-friendly message
+        }
     }
 
-    private void handleEditButton(MouseEvent event) {
-        // Logic for handling the "Edit" button click
-    }
-
-    private void handleDeleteButton(MouseEvent event) {
-        // Logic for handling the "Delete" button click
+    private void handleTableClick(MouseEvent event) {
+        // Logic for handling table clicks
     }
 }
