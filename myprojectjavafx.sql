b4a8ad55a14fe73c520c4d3b00bbd61f0a6175f9
LamiriAhmed <Lamiri202@gmail.com>
2024-01-24 21:04:43 +0100

    data base

--- myprojectjavafx.sql
+++ myprojectjavafx.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 05, 2023 at 11:15 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 24 jan. 2024 à 21:03
+-- Version du serveur : 10.4.27-MariaDB
+-- Version de PHP : 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `myprojectjavafx`
+-- Base de données : `myprojectjavafx`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Structure de la table `admin`
 --
 
 CREATE TABLE `admin` (
@@ -32,28 +32,32 @@ CREATE TABLE `admin` (
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `full_name` varchar(255) NOT NULL,
-  `DateBird` date DEFAULT NULL,
-  `image_path` varchar(100) DEFAULT NULL,
-  `Age` int(11) DEFAULT NULL,
-  `Ntele` varchar(20) DEFAULT NULL
+  `NTele` int(11) NOT NULL,
+  `DateBird` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `admin`
+-- Déchargement des données de la table `admin`
 --
 
-INSERT INTO `admin` (`id`, `password`, `email`, `full_name`, `DateBird`, `image_path`, `Age`, `Ntele`) VALUES
-(24, 'SaidSaid', 'Said@gmail.com', 'Said', '1968-12-05', 'C:\\Users\\Said\\Desktop\\ProjetFX\\login 2\\images\\admin_image_Said@gmail.com.jpg', 55, '0666666666'),
-(25, 'Said9Said9', 'Said9@gmail.com', 'Said9', '2023-12-05', 'C:UsersSaidDesktopProjetFXlogin 2srcmain\resourcesImages14.png', NULL, NULL),
-(26, 'Garnit6Garnit6', 'Garnit6@gmail.com', 'Garnit6', '2023-12-05', 'C:UsersSaidDesktopProjetFXlogin 2srcmain\resourcesImages14.png', NULL, NULL),
-(27, 'GarnitGarnit', 'Garnit@gmail.com', 'Garnit', '2006-12-05', 'C:UsersSaidDesktopProjetFXlogin 2srcmain\resourcesImages14.png', 17, NULL),
-(28, 'SaidSaid', 'Said@gmail.com', 'Said', '1968-12-05', 'C:\\Users\\Said\\Desktop\\ProjetFX\\login 2\\images\\admin_image_Said@gmail.com.jpg', 55, '0666666666'),
-(29, 'hamzahamza', 'hamza@gmail.com', 'hamza', '2001-12-05', 'C:UsersSaidDesktopProjetFXlogin 2srcmain\resourcesImages14.png', 22, '0622222222');
+INSERT INTO `admin` (`id`, `password`, `email`, `full_name`, `NTele`, `DateBird`) VALUES
+(8, 'said1234', 'said@gamil.com', 'said', 0, NULL),
+(9, '12345600', 'said@gmail', 'said', 0, NULL),
+(10, 'said1234', 'said@gmail.com', 'said', 0, NULL),
+(11, 'said12345', 'said@gmail.com', 'said', 0, NULL),
+(12, 'said1234', 'said@gamil.com', 'said', 0, NULL),
+(13, 'said1234', 'said@gmail.com', 'said', 0, NULL),
+(14, 'said1234', 'said@ff', 'said', 0, NULL),
+(15, 'said1234', 'said@gmail.com', 'said', 0, NULL),
+(16, 'said1234', 'said@gmail.com', 'said', 6, NULL),
+(17, '', '', '', 0, '0000-00-00'),
+(18, 'said1234', 'said@gmail.com', 'said', 0, NULL),
+(19, 'popo', 'popo', 'popo', 6060, '2023-11-08');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `production`
+-- Structure de la table `production`
 --
 
 CREATE TABLE `production` (
@@ -70,7 +74,7 @@ CREATE TABLE `production` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `production`
+-- Déchargement des données de la table `production`
 --
 
 INSERT INTO `production` (`id`, `metrage`, `Nom_de_race`, `Quantite`, `Qantite_Finale`, `Prix`, `Date_dentre`, `Nom_de_lemploye`, `Nom_de_fournisseur`, `origine`) VALUES
@@ -81,11 +85,8 @@ INSERT INTO `production` (`id`, `metrage`, `Nom_de_race`, `Quantite`, `Qantite_F
 (15, '23', 'Metrage', '34', '2', '3.0', '2023-12-04', 'TR', 'DOHA', 'FRRE'),
 (16, '23', 'ahmed', '34', '2', '3.0', '2023-12-04', 'FRRE', 'TR', 'DOHA'),
 (17, '3', 'Metrage', '3', '4', '5.0', '2023-12-04', 'T', 'AHMED', 'TTT'),
-(18, '3', 'Metrage', '3', '4', '5.0', '2023-12-04', 'T', 'AHMED', 'TTT'),
-(19, '1000', 'Metrage', '990', '880', '50000.0', '2023-12-04', 'taroudant', 'DOHA', 'ddd'),
-(21, '1000', 'Metrage', '990', '880', '50000.0', '2023-12-04', 'taroudant', 'DOHA', 'ddd'),
 (22, '100', 'Metrage', '99', '98', '55.0', '2023-12-04', 'AA', 'tAR', 'DOHA'),
-(24, '30', 'simo', '50', '70', '90.0', '2023-11-27', 'elmhjoub', 'AQ', 'AZE'),
+(24, '30', 'simo', '50', '70', '90.0', '2023-11-27', 'AZE', 'az', 'AQ'),
 (25, '30', 'simo', '50', '70', '90.0', '2023-11-27', 'elmhjoub', 'AQ', 'AZE'),
 (26, '30', 'simoppop', '50', '70', '90.0', '2023-11-27', 'AZE', 'elmhjoub', 'AQ'),
 (27, '30', 'simo', '50', '70', '90.0', '2023-11-27', 'elmhjoub', 'AQ', 'AZE'),
@@ -118,89 +119,117 @@ INSERT INTO `production` (`id`, `metrage`, `Nom_de_race`, `Quantite`, `Qantite_F
 (55, '1000', 'dajaji', '20000', '1500', '70000.0', '2023-12-05', 'SAID', 'laarbi', 'asaka'),
 (56, '1000', 'dajaji', '70', '60', '300.0', '2023-12-04', 'elmhjoub', 'hsika', 'TAROUDANT'),
 (57, '1000', 'aaaa', '20000', '1500', '70000.0', '2023-12-05', 'asaka', 'SAID', 'laarbi'),
-(58, '1000', 'said', '20000', '1500', '70000.0', '2023-12-05', 'laarbi', 'asaka', 'SAID'),
+(58, '1000', 'aaaa', '20000', '1500', '70000.0', '2023-12-05', 'asaka', 'SAID', 'laarbi'),
 (59, '1000', 'aaaa', '20000', '1500', '70000.0', '2023-12-05', 'asaka', 'SAID', 'laarbi'),
 (60, '1000', 'aaaa', '20000', '1500', '70000.0', '2023-12-05', 'asaka', 'SAID', 'laarbi'),
 (61, '1000', 'aaaa', '20000', '1500', '70000.0', '2023-12-05', 'asaka', 'SAID', 'jhzkjhzkjhzjkjzbi'),
-(66, '1000', 'AZZZZZZ', '20000', '1500', '70000.0', '2023-12-05', 'jhzkjhzkjhzjkjzbi', 'asaka', 'SAID'),
-(68, '2', 'Metrage99', '3', '45', '55.0', '2023-12-04', 'EEZ', 'TR', 'AHMED'),
-(69, '2', 'Metrage', '3', '45', '55.0', '2023-12-04', 'EEZ', 'TR', 'AHMED'),
-(70, '88', 'said', '99', '459', '55.09', '2023-12-04', 'DOHA', 'TR', 'AHMED');
-
-
---
--- Structure de la table `ventes`
---
-
-CREATE TABLE `ventes` (
-  `id` int(11) NOT NULL,
-  `Nom_De_Product` varchar(20) NOT NULL,
-  `Quantite` varchar(20) NOT NULL,
-  `Prix` varchar(20) NOT NULL,
-  `Date_Ventre` varchar(10) NOT NULL,
-  `Client` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Déchargement des données de la table `ventes`
-
-
-INSERT INTO `ventes` (`id`, `Nom_De_Product`, `Quantite`, `Prix`, `Date_Ventre`, `Client`) VALUES
-(1, 'Product1', '10', '100.0', '2023-12-05', 'Client1'),
-(2, 'Product2', '15', '150.0', '2023-12-05', 'Client2'),
-(3, 'Product3', '20', '200.0', '2023-12-05', 'Client3'),
-(4, 'Product4', '25', '250.0', '2023-12-05', 'Client4'),
-(5, 'Product5', '30', '300.0', '2023-12-05', 'Client5'),
-(6, 'Product6', '35', '350.0', '2023-12-05', 'Client6'),
-(7, 'Product7', '40', '400.0', '2023-12-05', 'Client7'),
-(8, 'Product8', '45', '450.0', '2023-12-05', 'Client8'),
-(9, 'Product9', '50', '500.0', '2023-12-05', 'Client9'),
-(10, 'Product10', '55', '550.0', '2023-12-05', 'Client10');
+(68, '1000', 'dajaji', '70', '60', '300.0', '2023-12-04', 'TAROUDANT', 'elmhjoub', 'hsika'),
+(69, '1000', 'dajaji', '70', '60', '300.0', '2023-12-04', 'TAROUDANT', 'elmhjoub', 'hsika'),
+(70, '1000', 'dajaji', '70', '60', '300.0', '2023-12-04', 'hsika', 'TAROUDANT', 'elmhjoub'),
+(71, '1000', 'dajaj', '30000', '20000', '600000.0', '2023-12-06', 'AHMED', 'hamza', 'taroudant'),
+(72, '1000', 'dajaj', '30000', '20000', '600000.0', '2023-12-06', 'DOHA', 'ghgh', 'taroudant'),
+(73, '1000', 'dajaj', '30000', '20000', '600000.0', '2023-12-06', 'DOHA', 'ghgh', 'taroudant'),
+(75, '30', 'rtret', '50', '70', '90.0', '2023-11-27', 'elmhjoub', 'AQ', 'AZE'),
+(79, '200', 'bibi', '1800', '1700', '5000.0', '2024-01-24', 'elmhjoub', 'AHMEDD', 'TAROUDANTT'),
+(80, '300', 'ronaldinyo', '1000', '9000', '3000.0', '2024-01-24', 'elmhjoub', 'ahmed', 'taroudant'),
+(81, '300', 'ronaldinyo', '1000', '9000', '3000.0', '2024-01-24', 'elmhjoub', 'ahmed', 'taroudant');
 
 -- --------------------------------------------------------
 
+--
+-- Structure de la table `suivi_production`
+--
+
+CREATE TABLE `suivi_production` (
+  `id` int(11) NOT NULL,
+  `quantite_par_jour` varchar(30) NOT NULL,
+  `date` varchar(30) NOT NULL,
+  `consomation` varchar(30) NOT NULL,
+  `emploiyee` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indexes for dumped tables
+-- Déchargement des données de la table `suivi_production`
+--
+
+INSERT INTO `suivi_production` (`id`, `quantite_par_jour`, `date`, `consomation`, `emploiyee`) VALUES
+(4, '34', '2023-06-06', '34', ''),
+(5, '34', '2023-06-06', '34', ''),
+(6, '34\'', '2023-12-13', '34', ''),
+(7, '324\'', '2023-12-13', '34', ''),
+(8, '20', '2023-12-11', '10', ''),
+(9, '12', '2023-12-11', '16', ''),
+(10, '12', '2023-12-04', '23', 'AHMED'),
+(11, '12', '2023-12-11', '45', 'elmhjoub'),
+(12, '12', '2023-12-12', '20', 'SAID'),
+(14, '120', '2023-12-11', '40', 'T'),
+(15, '120', '2023-12-11', '40', 'T'),
+(16, '120', '2023-12-11', '40', 'T'),
+(17, '120', '2023-12-11', '40', 'T'),
+(18, '120', '2023-12-11', '40', 'T'),
+(19, '12', '2023-12-12', '34', 'TAROUDANT'),
+(20, '12', '2023-12-12', '34', 'TAROUDANT'),
+(21, '12', '2023-12-12', '34', 'TAROUDANT'),
+(22, '23', '2023-12-12', '32', 'AZE'),
+(23, '23', '2023-12-12', '32', 'AZE'),
+(24, '12', '2023-12-12', '23', 'T'),
+(26, '23', '2023-12-04', '125', 'AHMED'),
+(27, '133', '2024-01-16', '23', 'elmhjoub'),
+(28, '150', '2024-01-08', '32', 'SAID'),
+(29, '150', '2024-01-17', '32', 'SAID'),
+(30, '150', '2024-01-17', '123', 'elmhjoub'),
+(31, '150', '2024-01-16', '123', 'elmhjoub'),
+(32, '150', '2024-01-16', '123', 'elmhjoub'),
+(33, '150', '2024-01-16', '123', 'TAROUDANT'),
+(34, '122', '2024-01-16', '134', 'AHMED'),
+(35, '122', '2024-01-16', '134', 'AHMED'),
+(36, '34', '2024-01-24', '5', 'elmhjoub'),
+(37, '20', '2024-01-24', '10', 'elmhjoub'),
+(38, '23', '2024-01-24', '9', 'elmhjoub');
+
+--
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `admin`
+-- Index pour la table `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `production`
+-- Index pour la table `production`
 --
 ALTER TABLE `production`
   ADD PRIMARY KEY (`id`);
 
--- Indexes for table `ventes`
 --
-ALTER TABLE `ventes`
+-- Index pour la table `suivi_production`
+--
+ALTER TABLE `suivi_production`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT pour la table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
-
--- AUTO_INCREMENT for table `ventes`
---
-ALTER TABLE `ventes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
--- AUTO_INCREMENT for table `production`
+-- AUTO_INCREMENT pour la table `production`
 --
 ALTER TABLE `production`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+
+--
+-- AUTO_INCREMENT pour la table `suivi_production`
+--
+ALTER TABLE `suivi_production`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
